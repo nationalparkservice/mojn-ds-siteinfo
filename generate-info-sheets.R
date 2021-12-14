e7d9f94f21ddc6359afbd7d4877e8194ade54fd0
Lehman <mlehman@nps.gov>
2021-12-13 16:15:59 -0800

    WY22 Updates

--- generate-info-sheets.R
+++ generate-info-sheets.R
@@ -3,8 +3,11 @@ library(pool)
 library(dbplyr)
 
 # Setup
-park <- "LAKE"
-dest.folder <- paste0("M:/MONITORING/DS_Water/Implementation/SitePackets/PreviousVisitInfo/", park) 
+park <- "MOJA"
+frame <- "Annual"  #"Annual" or "3Yr"
+wateryear <- "22"
+#dest.folder <- paste0("M:/MONITORING/DS_Water/Implementation/SitePackets/PreviousVisitInfo/", park) 
+dest.folder <- paste0("Data/SiteInfoSheets/",wateryear,"-",park)
 
 # Set up database connection
 db.params <- readr::read_csv("M:/MONITORING/DS_Water/Data/Database/ConnectFromR/ds-database-conn.csv") %>%
@@ -13,12 +16,12 @@ db.params$drv <- odbc::odbc()
 conn <- do.call(dbPool, db.params)
 
 # Get spring info from database
-springs <- tbl(conn, in_schema("export", "SiteInfoSheet")) %>%
+springs <- tbl(conn, in_schema("export", "SourceMostRecent")) %>%
   collect() %>%
-  select(ParkCode, SiteCode, VisitGroup) %>%
+  select(Park, SiteCode, FieldSeason, SampleFrame) %>%
   unique() %>%
-  filter(ParkCode == park) %>%
-  mutate(FileName = paste0(SiteCode, "_", VisitGroup, ".docx"))
+  filter(Park == park,SampleFrame == frame) %>%
+  mutate(FileName = paste0(SiteCode, "_", FieldSeason, ".docx"))
 
 # Close database connection
 poolClose(conn)
@@ -26,5 +29,6 @@ poolClose(conn)
 # Generate info sheets
 for (i in 1:nrow(springs)) {
   spring <- springs[i, ]
+  #This would be a good spot to add an option to overwrite existing files or not
   rmarkdown::render("site-info.Rmd", output_file = spring$FileName, output_dir = dest.folder, params = list(site.code = spring$SiteCode))
 }
