3a5afc14bc53be7a551c86a8dbb10916d3e17227
Moore <sierramoore@nps.gov>
2022-07-19 16:36:09 -0700

    Removed code that was being used for testing purposes.

--- dummycode.R
+++ dummycode.R
@@ -11,19 +11,9 @@ wateryear <- "2022"
 dest.folder <- paste0("Data/SiteInfoSheets/",wateryear,"-",park)
 
 #Install DesertSprings package and set up database
-install_github("nationalparkservice/mojn-ds-rpackage", ref = "master")
-conn1 <- OpenDatabaseConnection()
+#install_github("nationalparkservice/mojn-ds-rpackage", ref = "master")
+conn <- OpenDatabaseConnection()
 db <- GetRawData(conn)
-
-visit <- db$Visit
-
-spring.info1 <- db$Visit %>%
-  filter(SiteCode == params$site.code)
-
-visit.info1 <- spring.info1 %>%
-  #  select(-PhotoType, -PhotoLabel, -RenamedFilePath, -UtmX_m, -UtmY_m, -UTMZone, -PhotoNotes) %>%
-  unique()
-
 
 # Get spring info from database
 springs <- db$Visit %>%
@@ -36,5 +26,5 @@ springs <- db$Visit %>%
 for (i in 1:nrow(springs)) {
   spring <- springs[i, ]
   #This would be a good spot to add an option to overwrite existing files or not
-  rmarkdown::render("site-info.Rmd", output_file = spring$FileName, output_dir = dest.folder, params = list(site.code = spring$SiteCode))
+  rmarkdown::render("dummy-code.Rmd", output_file = spring$FileName, output_dir = dest.folder, params = list(site.code = spring$SiteCode))
 }
